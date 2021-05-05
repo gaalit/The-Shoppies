5a98197ed7f207b56771804b50eca5624797871d
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-05-05 16:06:55 -0400

    added conditional statement for edge case

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,6 +2,12 @@ import React from "react";
 import MovieItem from "./MovieItem";
 
 const MovieList = ({ movies, onMovieSelect }) => {
+  // conditional statement making sure no error is thrown if the search term isn't complete enough
+  if (!movies) {
+    return (
+      <div>Please make sure your search contains a minimum of 3 letters</div>
+    );
+  }
   const renderedList = movies.map((movie) => {
     return (
       <MovieItem
