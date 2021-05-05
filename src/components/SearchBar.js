ada7f152f9ff6cb5be4faf719eefedba35f9412a
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-05-05 11:12:20 -0400

    initial set-up of SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // TO DO: make sure we call callback from parent component
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Movie title</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
