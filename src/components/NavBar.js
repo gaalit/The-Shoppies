339bbcefd4d1791a33e9b922fa4806040fb91582
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-05-07 00:24:37 -0400

    created NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styling.css";
+
+const NavBar = () => {
+  return (
+    <section class="nav-bar">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            The Shoppies: Movie awards for entrepreneurs
+            <span class="icon">
+              <i class="trophy icon"></i>
+            </span>
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NavBar;
