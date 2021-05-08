2c773cb7dc5dfa64d620397f3f0ce6e42ebca761
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-05-08 11:36:18 -0400

    reverted back to previous code

--- src/apis/omdb.js
+++ src/apis/omdb.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = process.env.REACT_APP_MOVIE_API_TOKEN;
+const KEY = process.env.REACT_APP_MOVIE_API_KEY;
 
 export default axios.create({
   baseURL: "https://www.omdbapi.com/",
